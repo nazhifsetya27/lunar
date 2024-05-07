8d62aac0aa0f396ae2fc64c651e5046371421c1e
nazhifsetya27 <nazhif.sn@kalachakra.io>
2024-05-07 11:59:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-gray-700 display-xs-semibold text-center">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
